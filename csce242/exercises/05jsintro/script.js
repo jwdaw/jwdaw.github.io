2b87a491e882a7db93896c4c72ae53d6baa157db
Jackson Dawson <jwd2464@gmail.com>
2025-02-13 17:45:53 -0500

    messing around with different viewing sizes

--- csce242/exercises/05jsintro/script.js
+++ csce242/exercises/05jsintro/script.js
@@ -14,3 +14,34 @@ document.getElementById("btn-color").onclick = () => {
 document.getElementById("toggle-nav").onclick = () => {
   document.getElementById("nav-items").classList.toggle("hide-small");
 };
+
+document.getElementById("color-button").onclick = () => {
+  const color = document.getElementById("color").value.toLowerCase().trim();
+  let mood = "";
+  if (color == "blue") mood = "grumpy";
+  if (color == "yellow") mood = "mellow";
+  if (color == "red") mood = "angry";
+
+  document.getElementById("color-message").innerHTML = `You chose ${mood}`;
+};
+
+let pos = 0;
+const changeCirclePosition = (increment) => {
+  pos += increment;
+  document.getElementById("circle").style.setProperty("top", pos + "px");
+};
+
+document.getElementById("btn-down").onclick = () => {
+  changeCirclePosition(10);
+};
+
+document.getElementById("btn-up").onclick = () => {
+  changeCirclePosition(-10);
+};
+
+document.getElementById("btn-circle-color").onchange = (event) => {
+  const color = event.currentTarget.value;
+  document
+    .getElementById("circle")
+    .style.setProperty("background-color", color);
+};
